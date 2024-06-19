d22f6041ecbd8693143683b8a67d7c9ac8e3bd0a
luniemma <luniyisiemmanuel@gmail.com>
2024-06-19 10:42:35 -0400

    Add files via upload
    aks cluster deployments

---
+++ aks.tf
@@ -0,0 +1,69 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+#   backend "azurerm" {
+#     resource_group_name  = "emma-vm-vnet-migrations-rg"
+#     storage_account_name = "terraformstatexyz29"
+#     container_name       = "tfstate"
+#     key                  = "terraform.aks.tfstates"
+#   }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "aks_rg" {
+  name     = "aks-resource-group"
+  location = "eastus"
+}
+
+resource "azurerm_kubernetes_cluster" "aks_cluster" {
+  name                = "aks-cluster"
+  location            = azurerm_resource_group.aks_rg.location
+  resource_group_name = azurerm_resource_group.aks_rg.name
+  dns_prefix          = "aks-cluster"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_DS2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = {
+    Environment = "Production"
+  }
+}
+
+resource "azurerm_kubernetes_cluster_node_pool" "additional_node_pool" {
+  name                  = "additional"
+  kubernetes_cluster_id = azurerm_kubernetes_cluster.aks_cluster.id
+  vm_size               = "Standard_DS2_v2"
+  node_count            = 1
+
+  tags = {
+    Environment = "Production"
+  }
+}
+output "kube_config" {
+  value = azurerm_kubernetes_cluster.aks_cluster.kube_config_raw
+
+  sensitive = true
+}
+
+# az aks get-credentials --resource-group aks-resource-group --name aks-cluster
+# kubectl get nodes
+# kubectl create namespace argocd
+#kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+# kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}'
+# kubectl config set-context --current --namespace=argocd
+# kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 --decode
+# kubectl get svc argocd-server -n argocd
