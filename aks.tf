0c25325962d906060536f455f46c5df3cdb443de
luniemma <luniyisiemmanuel@gmail.com>
2024-06-20 10:40:07 -0400

    testing

--- aks.tf
+++ aks.tf
@@ -5,11 +5,11 @@ terraform {
       version = "~> 3.0"
     }
   }
- # backend "azurerm" {
-  #   resource_group_name  = "emma-vm-vnet-migrations-rg"
-  #   storage_account_name = "terraformstatexyz29"
-  #  container_name       = "tfstate"
-  #  key                  = "terraform.aks.tfstates"
+ backend "azurerm" {
+    resource_group_name  = "emma-vm-vnet-migrations-rg"
+    storage_account_name = "terraformstatexyz29"
+   container_name       = "tfstate"
+   key                  = "terraform.aks.tfstates"
    }
 }
 
